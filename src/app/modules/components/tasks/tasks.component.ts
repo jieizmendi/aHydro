9f90926e9b53c6185f69a211bbf1639eb4f7d09d
Juan I. Eizmendi <jieizmendi@gmail.com>
2018-11-09 17:44:01 -0300

    format task

--- src/app/modules/components/tasks/tasks.component.ts
+++ src/app/modules/components/tasks/tasks.component.ts
@@ -1,26 +1,8 @@
-import {
-  Component,
-  OnInit,
-  EventEmitter,
-  Output,
-  OnDestroy
-} from "@angular/core";
-import {
-  FormControl,
-  FormGroupDirective,
-  NgForm,
-  Validators
-} from "@angular/forms";
-import { ErrorStateMatcher } from "@angular/material/core";
-import { ModuleService } from "../../../core/services/module.service";
+import { Component, OnInit } from "@angular/core";
+import { FormControl, Validators } from "@angular/forms";
 import { TaskService } from "../../../core/services/task.service";
-import { Module } from "../../../core/models/module";
 import { Task } from "../../../core/models/task";
-import { MatSnackBar } from "@angular/material";
-import { Subscription } from "rxjs";
 import { Observable } from "rxjs";
-import { ActivatedRoute, ParamMap } from "@angular/router";
-import { Router } from "@angular/router";
 
 @Component({
   selector: "app-tasks",
@@ -32,10 +14,7 @@ export class TasksComponent implements OnInit {
   descriptionControl: FormControl;
   dateControl: FormControl;
 
-  constructor(
-    private taskService: TaskService
-  ) {
-  }
+  constructor(private taskService: TaskService) {}
 
   ngOnInit() {
     this.tasks$ = this.taskService.tasks$;
